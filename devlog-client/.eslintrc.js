6376e35b44f4c13d2fa09ab7123af768d998e2e1
unknown <qkrdmstlr3@naver.com>
2020-12-26 13:52:52 +0900

    [chore] eslint / prettier 설정
    - eslint / prettier관련 모듈 설치
    - 설정 파일 생성

---
+++ devlog-client/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prefer-const': 'off'
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  }
+}
