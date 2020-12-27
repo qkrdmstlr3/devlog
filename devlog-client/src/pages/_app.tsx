c2980726b4d37383a83157427d627913b6987358
unknown <qkrdmstlr3@naver.com>
2020-12-27 13:46:23 +0900

    [feat] @Components alias추가
    - babel과 tsconfig에 @Components alias를 추가하였다
    - app.tsx에서 레이아웃을 불러서 테스트를 해보았다

--- devlog-client/src/pages/_app.tsx
+++ devlog-client/src/pages/_app.tsx
@@ -8,6 +8,9 @@ import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from '@Style/GlobalStyle';
 import { theme } from '@Style/styled';
 
+// Components
+import Layout from '@Components/layout';
+
 export default class MyApp extends app {
   public render(): React.ReactElement {
     const { Component, pageProps } = this.props;
@@ -23,7 +26,9 @@ export default class MyApp extends app {
             content="조개소년, shellboy, devlog, 개발 블로그"
           />
         </Helmet>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
         <GlobalStyle />
       </ThemeProvider>
     );
