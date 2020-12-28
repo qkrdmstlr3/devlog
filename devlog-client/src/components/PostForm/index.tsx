2a61664f505da0c179fa063cabacb2ea5bc98d59
unknown <qkrdmstlr3@naver.com>
2020-12-29 00:15:59 +0900

    [feat] PostForm 컴포넌트 생성
    - 글 수정 / 생성 시 공통으로 사용할 컴포넌트를 만들었다

---
+++ devlog-client/src/components/PostForm/index.tsx
@@ -0,0 +1,85 @@
+// Dependencies
+import React, { useState } from 'react';
+import * as Style from './styled';
+
+// Types
+import { FormValueType } from '../../common/types';
+
+interface PostFormProps {
+  /** page 이름 */
+  page: string;
+  /** 수정할 때 현재 포스트의 리스트 이름 */
+  listName?: string;
+  /** 수정할 때 현재 포스트의 이름 */
+  title?: string;
+  /** 수정할 때 현재 포스트의 내용 */
+  content?: string;
+  /** submit할 때 호출될 함수 */
+  submitHandler: (
+    event: React.FormEvent<HTMLFormElement>,
+    formValue: FormValueType
+  ) => void;
+}
+
+const dummyItems = [
+  { id: 1, title: 'Network', postCount: 10 },
+  { id: 2, title: 'Database', postCount: 10 },
+];
+
+function PostForm({
+  page,
+  listName = '',
+  title = '',
+  content = '',
+  submitHandler,
+}: PostFormProps): React.ReactElement {
+  const [formValue, setFormValue] = useState({
+    titleValue: title,
+    contentValue: content,
+    listNameValue: listName,
+  });
+
+  const changeHandler = (
+    event: React.FormEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >,
+    name: string
+  ) => {
+    setFormValue({
+      ...formValue,
+      [name]: event.currentTarget.value,
+    });
+  };
+
+  return (
+    <Style.Form onSubmit={(event) => submitHandler(event, formValue)}>
+      <Style.Header>
+        <Style.Dropdown
+          value={formValue.listNameValue}
+          onChange={(event) => changeHandler(event, 'listNameValue')}
+        >
+          {dummyItems.map((item) => (
+            <option key={item.title} value={item.title}>
+              {item.title}
+            </option>
+          ))}
+        </Style.Dropdown>
+        <Style.PageTitle>{page}</Style.PageTitle>
+      </Style.Header>
+      <Style.TitleInput
+        type="text"
+        placeholder="제목 입력"
+        value={formValue.titleValue}
+        onChange={(event) => changeHandler(event, 'titleValue')}
+      />
+      <Style.ContentInput
+        placeholder="내용 입력"
+        value={formValue.contentValue}
+        onChange={(event) => changeHandler(event, 'contentValue')}
+      />
+      <Style.SubmitButton>제출하기</Style.SubmitButton>
+    </Style.Form>
+  );
+}
+
+export default PostForm;
