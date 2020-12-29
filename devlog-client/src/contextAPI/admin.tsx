a9782acd059c74020527254853f163a7352f1cb1
unknown <qkrdmstlr3@naver.com>
2020-12-29 13:32:02 +0900

    [feat] AdminContextAPI 생성
    - 관리자 권한을 얻기 위한 AdminContextAPI를 만듦
    - 서버에서 따로 관리할 필요 없이, 나 혼자이기 때문에 이렇게 하면
      좋겠다고 판단

---
+++ devlog-client/src/contextAPI/admin.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useState } from 'react';
+import config from '@Config/index';
+
+interface InitialStateType {
+  adminKey: string;
+  getAdmin: (password: string) => void;
+}
+
+interface AdminKeyProviderType {
+  children: React.ReactNode;
+}
+
+const initialState = {
+  adminKey: '',
+  getAdmin: () => null,
+};
+
+const AdminContext = createContext<InitialStateType>(initialState);
+
+const AdminProvider = ({ children }: AdminKeyProviderType): any => {
+  const [adminKey, setAdminKey] = useState<string>('');
+
+  const getAdmin = (password: string) => {
+    if (config.secretKey === password) {
+      setAdminKey(config.secretKey);
+    }
+  };
+
+  const context = {
+    adminKey,
+    getAdmin,
+  };
+
+  return (
+    <AdminContext.Provider value={context}>{children}</AdminContext.Provider>
+  );
+};
+
+export { AdminProvider, AdminContext };
