84e86ea8d08c78de96bd1acfce635be3e3ac8bf4
unknown <qkrdmstlr3@naver.com>
2020-12-30 12:14:52 +0900

    [chore] eslint 설정
    - eslint 설치 및 설정파일 생성

--- devlog-server/src/index.ts
+++ devlog-server/src/index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
 app.listen(4000, () => {
-    console.log('Hello, World!');
+  console.log('hello World');
 });
