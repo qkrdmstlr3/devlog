d150bce82045c0f9764e7b00c7e6322b848857ec
박은식 <qkrdmstlr3@naver.com>
2020-12-30 19:52:17 +0900

    [refactor] test용 resolver와 graphql제거
    - 테스트하기 위해서 만든 resolver와 graphql을 제거하였다

--- devlog-server/src/graphql/resolvers/test.ts
+++
@@ -1,9 +0,0 @@
-import { IResolvers } from 'graphql-tools';
-const resolverMap: IResolvers = {
-  Query: {
-    helloWorld(_: void, args: void): string {
-      return `Hello GraphQL~!`;
-    },
-  },
-};
-export default resolverMap;
