01189dddbf51b2f9811a7c344ddeed690ee38337
박은식 <qkrdmstlr3@naver.com>
2021-01-03 20:46:51 +0900

    [feat] _app.tsx파일에 apolloClient연동
    - apolloProvider를 사용해서 만들어둔 아폴로 모듈을 넣어주었다
    - useMemo hook을 사용하기 위해서 next의 app을 extends해서 쓰는 클래스를 버림
      - 다른 것들과 통일되게 함수 형태로 만듦

--- devlog-client/src/pages/_app.tsx
+++ devlog-client/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 // Dependencies
-import app from 'next/app';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
@@ -14,29 +13,41 @@ import Layout from '@Components/Layout';
 // Contexts
 import { AdminProvider } from '@ContextAPI/admin';
 
-export default class MyApp extends app {
-  public render(): React.ReactElement {
-    const { Component, pageProps } = this.props;
+// Apollo
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '@Libs/apolloClient';
 
-    return (
-      <ThemeProvider theme={theme}>
-        <Helmet>
-          <title>Shellboy DevLog</title>
-          <meta charSet="utf-8" />
-          <meta name="description" content="조개소년 개발 블로그" />
-          <meta
-            name="Keywords"
-            content="조개소년, shellboy, devlog, 개발 블로그"
-          />
-          <link rel="shortcut icon" href="/favicon.ico" />
-        </Helmet>
-        <AdminProvider>
+interface AppProps {
+  Component: any;
+  pageProps: any;
+}
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps): React.ReactElement {
+  const apolloClient = useApollo(pageProps);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Helmet>
+        <title>Shellboy DevLog</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="조개소년 개발 블로그" />
+        <meta
+          name="Keywords"
+          content="조개소년, shellboy, devlog, 개발 블로그"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Helmet>
+      <AdminProvider>
+        <ApolloProvider client={apolloClient}>
           <Layout>
             <Component {...pageProps} />
           </Layout>
-        </AdminProvider>
-        <GlobalStyle />
-      </ThemeProvider>
-    );
-  }
+        </ApolloProvider>
+      </AdminProvider>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
