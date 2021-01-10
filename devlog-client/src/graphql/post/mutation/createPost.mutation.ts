f5317b85607e633f446b64307a7de64fb09d2ce5
박은식 <qkrdmstlr3@naver.com>
2021-01-10 19:39:42 +0900

    [feat | front] createPost mutation 생성
    - 포스트를 생성하는 createPost mutation을 만듦

---
+++ devlog-client/src/graphql/post/mutation/createPost.mutation.ts
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export default gql`
+  mutation createPost($title: String!, $content: String!, $listId: Int!) {
+    createPost(input: { title: $title, content: $content, listId: $listId }) {
+      id
+      title
+      content
+      createdAt
+    }
+  }
+`;
