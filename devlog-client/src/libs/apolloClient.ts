061977c700fa1d616b7e20607fa76023f475d470
박은식 <qkrdmstlr3@naver.com>
2021-01-13 12:56:54 +0900

    [refactor | front] apolloClient파일 수정
    - 기존방식대로 하면 getPost를 할 때 query문이 실행된 이후 캐시에 저장이 되었지만, 같은 페이지로 들어갈 경우 이미 불러온 적이 있는 포스트를 다시 서버로 요청을 보내는 문제가 발생하였다.
    - 공식 github에 있는 예제를 보고 addApolloState를 추가하여서 ssr사용 시 props의 상태로 전달해주었다.
    - 처음에 초기화할 떄에도 useApollo에 pageProps값을 전달해서 apollo cache상태를 넣는 방식으로 변경하였다.

--- devlog-client/src/libs/apolloClient.ts
+++ devlog-client/src/libs/apolloClient.ts
@@ -11,9 +11,12 @@ import {
   NormalizedCacheObject,
 } from '@apollo/client';
 import merge from 'deepmerge';
+import isEqual from 'lodash/isEqual';
 
 // Config
 import config from '@Config/index';
+
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
 
 let apolloClient: ApolloClient<NormalizedCacheObject>;
 
@@ -35,20 +38,37 @@ export function initilizeApollo(
   if (initialState) {
     const existingCache = _apolloClient.extract();
 
-    const data = merge(initialState as any, existingCache);
+    const data = merge(initialState as any, existingCache, {
+      arrayMerge: (destinationArray, sourceArray) => [
+        ...sourceArray,
+        ...destinationArray.filter((d) =>
+          sourceArray.every((s) => !isEqual(d, s))
+        ),
+      ],
+    });
 
     _apolloClient.cache.restore(data);
   }
 
-  if (typeof window === 'undefined') return _apolloClient;
   if (!apolloClient) apolloClient = _apolloClient;
 
   return _apolloClient;
 }
 
-export function useApollo(
-  initialState = null
-): ApolloClient<NormalizedCacheObject> {
-  const store = useMemo(() => initilizeApollo(initialState), [initialState]);
+export function addApolloState(
+  client: ApolloClient<NormalizedCacheObject>,
+  pageProps: any
+): any {
+  if (pageProps?.props) {
+    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
+  }
+
+  console.log(pageProps);
+  return pageProps;
+}
+
+export function useApollo(pageProps: any): ApolloClient<NormalizedCacheObject> {
+  const state = pageProps[APOLLO_STATE_PROP_NAME];
+  const store = useMemo(() => initilizeApollo(state), [state]);
   return store;
 }
