e5eae86db4763dc8bf783316e77f648b36fd067a
박은식 <qkrdmstlr3@naver.com>
2021-01-19 12:19:56 +0900

    [feat] next js bundle analyzer추가
    - next.config.js 파일 추가 및 next js bundle analyzer설정

---
+++ devlog-client/next.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  pageExtensions: ['tsx'],
+  webpack(config) {
+    let prod = process.env.NODE_ENV === 'production';
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+    };
+  },
+});
