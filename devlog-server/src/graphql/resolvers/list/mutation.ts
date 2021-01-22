f38a1c811127fc16eb08fa1a7ba798241464158d
박은식 <qkrdmstlr3@naver.com>
2021-01-22 15:46:33 +0900

    [fix | server] list 삭제시에 리턴값 변경
    - list삭제할 때 프론트에서 apollo cache처리를 위해서 삭제한 list의 Id를 반환한다

--- devlog-server/src/graphql/resolvers/list/mutation.ts
+++ devlog-server/src/graphql/resolvers/list/mutation.ts
@@ -10,12 +10,12 @@ export const createListMutation = async (title: string): Promise<List> => {
   return list;
 };
 
-export const deleteListMutation = async (listId: number): Promise<boolean> => {
+export const deleteListMutation = async (listId: number): Promise<number> => {
   const list = await List.findOne({ where: { id: listId } });
   if (!list) {
     throw new GraphQLError('존재하지 않는 리스트를 삭제하려 하셨습니다');
   }
 
   await list.remove();
-  return true;
+  return listId;
 };
