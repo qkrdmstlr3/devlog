5686181802d471188653fef569cc31b86cb04660
박은식 <qkrdmstlr3@naver.com>
2021-01-29 17:09:37 +0900

    [feat | gatsby] page template 구현
    - page들의 template이 될 컴포넌트를 구현하였다

---
+++ devlog-gatsby/src/templates/PostTemplate.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import { ITemplateProps } from '../interface';
+
+type IPostTemplateProps = ITemplateProps<{
+  html: string;
+  title: string;
+}>;
+
+const PostTemplate = (props: IPostTemplateProps): React.ReactElement => {
+  return (
+    <Layout>
+      <code>
+        <pre>{JSON.stringify(props, null, 4)}</pre>
+      </code>
+    </Layout>
+  );
+};
+
+export default PostTemplate;
