bb9f9ecb0cb171010efaab6044bd02185dd3fa6c
박은식 <qkrdmstlr3@naver.com>
2021-01-30 16:20:34 +0900

    [feat | gatsby] font추가 및 storybook에 globalStyle설정
    - storybook에 globalStyle을 설정해줌
    - 둥근모 폰트추가

--- devlog-gatsby/.storybook/preview.js
+++ devlog-gatsby/.storybook/preview.js
@@ -1,3 +1,47 @@
+// Dependencies
+import React from 'react';
+import { Global, css } from '@emotion/core';
+import emotionReset from 'emotion-reset';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      ${emotionReset}
+
+      * {
+        box-sizing: border-box;
+        font-family: 'DungGeunMo';
+      }
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+      button,
+      button:active {
+        outline: none;
+        cursor: pointer;
+      }
+      input:focus {
+        outline: none;
+      }
+
+      @font-face {
+        font-family: 'DungGeunMo';
+        src: url('../src/common/fonts/DungGeunMo.ttf');
+      }
+    `}
+  />
+);
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Story />
+      <GlobalStyle />
+    </>
+  ),
+];
