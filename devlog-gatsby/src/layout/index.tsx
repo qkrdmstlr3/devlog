0bf72d3ee1e7e5eeb5044b7659d1fabb5c1956d4
박은식 <qkrdmstlr3@naver.com>
2021-02-03 21:26:23 +0900

    [feat | gatsby] layout에 footer추가
    - layout에 footer를 추가하였다

--- devlog-gatsby/src/layout/index.tsx
+++ devlog-gatsby/src/layout/index.tsx
@@ -11,6 +11,14 @@ function Layout({ children }: LayoutProps) {
     <Style.Layout>
       <GlobalStyle />
       {children}
+      <Style.Footer>
+        <Style.Copyright>Copyright © 2021 Shellboy</Style.Copyright>
+        <Style.Information>
+          <Style.Github href="https://github.com/qkrdmstlr3">
+            Github
+          </Style.Github>
+        </Style.Information>
+      </Style.Footer>
     </Style.Layout>
   );
 }
--- devlog-gatsby/src/layout/styled.ts
+++ devlog-gatsby/src/layout/styled.ts
@@ -2,8 +2,28 @@
 import styled from '../common/style/styled';
 
 export const Layout = styled.main`
+  position: relative;
   max-width: 1200px;
   height: 100vh;
   margin: 0 auto;
+  padding-top: 30px;
+`;
+
+export const Footer = styled.footer`
+  position: absolute;
+  bottom: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  opacity: 0.8;
   font-family: neodgm, sans-serif;
+`;
+
+export const Copyright = styled.span``;
+
+export const Information = styled.div``;
+
+export const Github = styled.a`
+  text-decoration: underline;
 `;
