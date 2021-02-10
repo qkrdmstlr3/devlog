c077097795fff881cf1fee52de567e1d057f65c4
박은식 <qkrdmstlr3@naver.com>
2021-02-10 21:24:16 +0900

    [feat] 타이핑 효과를 위해서 typingHook을 구현
    - 글자가 타이핑되는 효과를 주기 위해서 typingHook을 구현해보았다.
    - 타이핑이 끝나는 시점을 알기 위해서 boolean type변수도 추가

---
+++ src/hooks/typingHook.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+interface TypingHookProps {
+  content: string;
+}
+
+function sleep(t: number) {
+  return new Promise((resolve) => setTimeout(resolve, t));
+}
+
+function typingHook({ content }: TypingHookProps) {
+  const [text, setText] = useState<string>('');
+  const [isTypingEnd, setIsTypingEnd] = useState<boolean>(false);
+
+  const typingText = async () => {
+    for (let i = 0; i <= content.length; i += 1) {
+      await sleep(100);
+      setText(content.slice(0, i));
+    }
+    setIsTypingEnd(true);
+  };
+
+  useEffect(() => {
+    typingText();
+  }, []);
+
+  return [text, isTypingEnd];
+}
+
+export default typingHook;
