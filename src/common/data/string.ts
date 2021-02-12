ca135ba269a3876f7e84c96099e9be8111fbcf15
박은식 <qkrdmstlr3@naver.com>
2021-02-12 17:45:21 +0900

    [feat] recoil game state에 gameStatus key값 추가 및 설명 추가, 그에 따른 string값 추가
    - recoil game state에 현재 게임의 진행 상황을 나타낸다
    	- 주석을 이용해서 어느정도 진행되었는지를 나타내었다
    - string파일을 만들어서 해당 게임상황에 보여지는 텍스트 문구를 나타내었다

--- src/common/data/string.ts
+++ src/common/data/string.ts
@@ -1,5 +1,3 @@
-import { atomFamily, atom, selector } from 'recoil';
-
 /**
  * Current Status : 현재 게임 상태
  * 1 : 초기 렌더링 (야생의 ~가 나타났다!) [TextBox Component]
@@ -8,14 +6,12 @@ import { atomFamily, atom, selector } from 'recoil';
  * 4 : 상대 포켓몬 공격 [TextBox Component]
  * 5 : 내 포켓몬의 닳은 체력 [TextBox Component]
  * 6 : 내 포켓몬 공격 [TextBox Component]
- * 7 : 적 포켓몬의 달흔 체력 [TextBox Component]
+ * 7 : 적 포켓몬의 닳은 체력 [TextBox Component]
  * 8 : 내 포켓몬 교체 [TextBox Component]
  */
 
-export const gameState = atom({
-  key: 'gameState',
-  default: {
-    loading: true,
-    gameStatus: 1,
-  },
-});
+export const textData = {
+  1: () => '앗! 야생의\n 리액트(이)가 나타났다!',
+  2: () => '가랏! 리액트!',
+  3: () => '', // SelectBox Component
+};
