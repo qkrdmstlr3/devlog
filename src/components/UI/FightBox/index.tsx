f6ebbb05cce8f762fc9036fa0dafbb8aef7df406
박은식 <qkrdmstlr3@naver.com>
2021-02-18 19:05:24 +0900

    [feat] FightBox UI Component 세부 구현
    - FightBox UI Component의 세부사항들을 구현하였다

--- src/components/UI/FightBox/index.tsx
+++ src/components/UI/FightBox/index.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+// Dependencies
+import React, { useState } from 'react';
+import * as Style from './styled';
+
+// Recoil
+import { useRecoilState } from 'recoil';
+import { gameState } from '../../../lib/recoil/game';
+
+// Components
+import BorderBox from '../BorderBox';
+
+// Data
+import { MyPokemon } from '../../../common/data/pokemon';
 
 function FightBox() {
-  return <div></div>;
+  const [currentSkill, setCurrentSkill] = useState(0);
+  const [{ name }, setGameState] = useRecoilState(gameState);
+
+  const mouseEnterHandler = (index: number) => {
+    if (index === currentSkill || MyPokemon[name].skill[index].name === '-')
+      return;
+    setCurrentSkill(index);
+  };
+
+  const clickHandler = (index: number) => {
+    // gameStatus 5번으로 설정 후 textBox에서 6 7 8일 경우에 따라서 설정
+  };
+
+  return (
+    <Style.Wrapper>
+      <Style.LeftWrapper>
+        <BorderBox>
+          <Style.SkillBox>
+            {MyPokemon[name].skill.map((skill, index) => (
+              <Style.Skill
+                key={index}
+                onMouseEnter={() => mouseEnterHandler(index)}
+                onClick={() => clickHandler(index)}
+              >
+                {index === currentSkill && <Style.Select>▶</Style.Select>}
+                {skill.name}
+              </Style.Skill>
+            ))}
+          </Style.SkillBox>
+        </BorderBox>
+      </Style.LeftWrapper>
+      <Style.RightWrapper>
+        <BorderBox>
+          <Style.SkillTypeBox>
+            기술 타입 / {MyPokemon[name].skill[currentSkill].skillType}
+          </Style.SkillTypeBox>
+        </BorderBox>
+      </Style.RightWrapper>
+    </Style.Wrapper>
+  );
 }
 
 export default FightBox;
