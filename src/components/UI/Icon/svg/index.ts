19e123eabdf829307e473c08c0e8a70bfc4b81f3
박은식 <qkrdmstlr3@naver.com>
2021-02-23 21:30:29 +0900

    [feat] graphql svg icon추가 및 내 포켓몬에 graphql추가
    - graphql svg icon을 추가하였다
    - recoil myPokemon state에 graphql을 추가해주었다

--- src/components/UI/Icon/svg/index.ts
+++ src/components/UI/Icon/svg/index.ts
@@ -1,2 +1,3 @@
 export { default as rightChevron } from './Chevron';
 export { default as react } from './React';
+export { default as graphql } from './Graphql';
--- src/lib/recoil/myPokemon.ts
+++ src/lib/recoil/myPokemon.ts
@@ -16,5 +16,18 @@ export const myPokemonState = atom({
         { name: '-', damage: 0 },
       ],
     },
+    graphql: {
+      sort: 'graphql',
+      name: '큐엘',
+      level: 50,
+      fullHP: 20,
+      currentHP: 20,
+      skill: [
+        { name: '스킬1', damage: 3, skillType: '노멀' },
+        { name: '스킬2', damage: 5, skillType: '노멀' },
+        { name: '-', damage: 0 },
+        { name: '-', damage: 0 },
+      ],
+    },
   },
 });
