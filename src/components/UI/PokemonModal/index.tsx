4c40fcfc2e85cc12f4dbe7fcd5faae1e42c8709d
박은식 <qkrdmstlr3@naver.com>
2021-02-23 13:56:34 +0900

    [feat] PokemonModal UI Component 생성
    - 내 포켓몬의 리스트를 보여줄 PokemonModal UI Component를 생성

---
+++ src/components/UI/PokemonModal/index.tsx
@@ -0,0 +1,17 @@
+// Dependencies
+import React from 'react';
+import * as Style from './styled';
+import { useRecoilState } from 'recoil';
+import { gameState } from '../../../lib/recoil/game';
+
+// Data
+import { MyPokemon } from '../../../common/data/pokemon';
+
+// Component
+import Icon from '../Icon';
+
+function PokemonListModal() {
+  return <Style.Wrapper></Style.Wrapper>;
+}
+
+export default PokemonListModal;
---
+++ src/components/UI/PokemonModal/styled.ts
@@ -0,0 +1,6 @@
+import styled from '../../../common/style/styled';
+
+export const Wrapper = styled.div`
+  position: absolute;
+  width: calc(100% - 50px);
+`;
