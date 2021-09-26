f687422bf67655d60e705ad02018833b556a4dbb
parkeunsik <qkrdmstlr3@naver.com>
2021-09-26 22:48:06 +0900

    [feat] gatsby-node.js로 md페이지 구현
    - gatsby-node.js를 이용해서 md를 페이지로 만들어주었다
    - 임시적으로 PostPage컴포넌트를 만들어주었다

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,60 @@
-require('ts-node').register();
+const path = require("path");
 
-const { createPages } = require('./src/lib/createPages');
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
 
-exports.createPages = createPages;
+  if (node.internal.type === "MarkdownRemark") {
+    const fileNode = getNode(node.parent);
+    const parsedFilePath = path.parse(fileNode.relativePath);
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: parsedFilePath.name,
+    });
+  }
+};
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const { data, errors } = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            rawMarkdownBody
+            frontmatter {
+              title
+              date
+              category
+            }
+          }
+        }
+      }
+    }
+  `);
+  if (errors || !data) {
+    throw errors;
+  }
+
+  data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: `${String(node.frontmatter.category)}/${String(
+        node.frontmatter.title
+      )}`,
+      context: {
+        markdown: node.rawMarkdownBody,
+        html: node.html,
+        title: String(node.frontmatter.title),
+        date: String(node.frontmatter.date),
+        category: String(node.frontmatter.category),
+      },
+      component: path.resolve(
+        __dirname,
+        "./src/components/templates/post-page/PostPage.js"
+      ),
+    });
+  });
+};
