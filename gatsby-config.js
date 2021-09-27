627bb859f9fe67a1b06b322712c29dc18fa66006
parkeunsik <qkrdmstlr3@naver.com>
2021-09-27 23:00:45 +0900

    [chore] gatsby-config 불필요한 plugin 제거
    - gatsby-config파일에서 불필요한 plugin들을 제거해주었다

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,44 +3,37 @@ module.exports = {
     title: `조개소년 개발블로그`,
     description: `개발자 조개소년의 개발블로그`,
     author: `shellboy`,
-    siteUrl: 'https://shellboylog.com',
+    siteUrl: "https://shellboylog.com",
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'shellboy devlog',
-        short_name: 'shellboy devlog',
-        start_url: '/',
-        icon: 'posts/icon/favicon.png',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'contents',
-        path: `${__dirname}/posts/contents`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'fonts',
-        path: `${__dirname}/posts/fonts`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-generate-typings`,
-      options: {
-        dest: `./src/graphql-types.d.ts`,
-      },
-    },
     `gatsby-plugin-emotion`,
-    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "shellboy devlog",
+        short_name: "shellboy devlog",
+        start_url: "/",
+        icon: "posts/icon/favicon.png",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "contents",
+        path: `${__dirname}/posts/contents`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "fonts",
+        path: `${__dirname}/posts/fonts`,
+      },
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -48,9 +41,6 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
               maxWidth: 590,
             },
           },
@@ -64,17 +54,17 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: 'https://shellboylog.com',
-        sitemap: 'https://shellboylog.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }],
+        host: "https://shellboylog.com",
+        sitemap: "https://shellboylog.com/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
     {
-      resolve: 'gatsby-plugin-nprogress',
+      resolve: "gatsby-plugin-nprogress",
       options: {
-        color: 'tomato',
+        color: "tomato",
         showSpinner: false,
         minimum: 0.5,
       },
