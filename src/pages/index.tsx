055eb56cf299050fa90b54a0461869c1c07d31b5
parkeunsik <qkrdmstlr3@naver.com>
2022-04-30 23:17:16 +0900

    [feat] index page에서 query로 받은 데이터 파싱
    - obj를 flat하게 펴기 위한 flatObj module구현
    - index page에서 받은 모듈을 이용해 데이터 파싱

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,55 @@
 // Dependencies
-import React from "react";
+import React from 'react';
+import { graphql } from 'gatsby';
 
-const Index = (): React.ReactElement => {
-  return <div>hello</div>;
-};
+// Components
+import MainPage, { PostMetaDataType } from '../components/pages/Main';
 
-export default Index;
+// Utils
+import { flatObj } from '../utils/module';
+
+interface MainProps {
+  data: {
+    allMarkdownRemark: {
+      edges: {
+        node: {
+          excerpt: string;
+          frontmatter: {
+            category: string;
+            date: string;
+            title: string;
+          };
+        };
+      }[];
+    };
+  };
+}
+
+function Main({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}: MainProps): React.ReactElement {
+  const metaData = edges.map((edge) => flatObj<PostMetaDataType>(edge.node));
+
+  return <MainPage postMetaData={metaData} />;
+}
+
+export const query = graphql`
+  query PostMetaData {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      edges {
+        node {
+          excerpt(truncate: true, pruneLength: 200)
+          frontmatter {
+            title
+            date(formatString: "YYYY-MM-DD")
+            category
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default Main;
