073b9c88acc7e4ce11103ff376c3af4504a7db58
parkeunsik <qkrdmstlr3@naver.com>
2022-04-30 22:17:35 +0900

    [chore] rs > ts 변경 위해서 프로젝트 설정 초기화
    - rescript에서 typescript로의 마이그레이션을 위한 초기화

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,60 +1,60 @@
-const path = require("path");
+// const path = require("path");
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions;
 
-  if (node.internal.type === "MarkdownRemark") {
-    const fileNode = getNode(node.parent);
-    const parsedFilePath = path.parse(fileNode.relativePath);
+//   if (node.internal.type === "MarkdownRemark") {
+//     const fileNode = getNode(node.parent);
+//     const parsedFilePath = path.parse(fileNode.relativePath);
 
-    createNodeField({
-      node,
-      name: "slug",
-      value: parsedFilePath.name,
-    });
-  }
-};
+//     createNodeField({
+//       node,
+//       name: "slug",
+//       value: parsedFilePath.name,
+//     });
+//   }
+// };
 
-exports.createPages = async ({ actions, graphql }) => {
-  const { createPage } = actions;
+// exports.createPages = async ({ actions, graphql }) => {
+//   const { createPage } = actions;
 
-  const { data, errors } = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            html
-            rawMarkdownBody
-            frontmatter {
-              title
-              date
-              category
-            }
-          }
-        }
-      }
-    }
-  `);
-  if (errors || !data) {
-    throw errors;
-  }
+//   const { data, errors } = await graphql(`
+//     query {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             html
+//             rawMarkdownBody
+//             frontmatter {
+//               title
+//               date
+//               category
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
+//   if (errors || !data) {
+//     throw errors;
+//   }
 
-  data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: `${String(node.frontmatter.category)}/${String(
-        node.frontmatter.title
-      )}`,
-      context: {
-        markdown: node.rawMarkdownBody,
-        html: node.html,
-        title: String(node.frontmatter.title),
-        date: String(node.frontmatter.date),
-        category: String(node.frontmatter.category),
-      },
-      component: path.resolve(
-        __dirname,
-        "./src/components/templates/post-page/PostPage.js"
-      ),
-    });
-  });
-};
+//   data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `${String(node.frontmatter.category)}/${String(
+//         node.frontmatter.title
+//       )}`,
+//       context: {
+//         markdown: node.rawMarkdownBody,
+//         html: node.html,
+//         title: String(node.frontmatter.title),
+//         date: String(node.frontmatter.date),
+//         category: String(node.frontmatter.category),
+//       },
+//       component: path.resolve(
+//         __dirname,
+//         "./src/components/templates/post-page/PostPage.js"
+//       ),
+//     });
+//   });
+// };
