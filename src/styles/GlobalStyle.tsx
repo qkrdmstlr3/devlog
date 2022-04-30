fd34eaf6723fe7d4a1924c03082fc04eaa6cf14e
parkeunsik <qkrdmstlr3@naver.com>
2022-04-30 22:18:36 +0900

    [refactor] 전역 css설정 변경
    - 기존 css에서 emotion을 사용

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Global, css } from '@emotion/react';
+
+function GlobalStyle(): React.ReactElement {
+  return (
+    <Global
+      styles={css`
+        @import url('//cdn.jsdelivr.net/gh/Dalgona/neodgm-webfont@1.510/neodgm/style.css');
+
+        html {
+          height: 100%;
+        }
+        body {
+          margin: 0;
+          background-color: #ddd;
+          height: 100%;
+        }
+        * {
+          box-sizing: border-box;
+        }
+        a {
+          text-decoration: none;
+          color: inherit;
+        }
+        button,
+        button:active {
+          outline: none;
+          cursor: pointer;
+        }
+        input:focus {
+          outline: none;
+        }
+      `}
+    />
+  );
+}
+
+export default GlobalStyle;
