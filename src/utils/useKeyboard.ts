3a86059cc786a1cd347c938f7ef046560f1e80f2
shellboy <qkrdmstlr3@naver.com>
2022-08-15 15:53:09 +0900

    feat: useKeyboard hook 생성

---
+++ src/utils/useKeyboard.ts
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+
+type KeyType = 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight' | 'Escape' | 'Space';
+interface KeyEvent {
+  key: KeyType;
+  keyEvent: (event: KeyboardEvent) => void;
+}
+
+interface KeyboardHandlerProps {
+  keyEvents: KeyEvent[];
+}
+
+const keyMapper: { [key in KeyType]: string } = {
+  ArrowUp: 'ArrowUp',
+  ArrowDown: 'ArrowDown',
+  ArrowLeft: 'ArrowLeft',
+  ArrowRight: 'ArrowRight',
+  Escape: 'Escape',
+  Space: ' ',
+};
+
+const useKeyboard = ({ keyEvents }: KeyboardHandlerProps) => {
+  const executeEvents = (event: KeyboardEvent) => {
+    console.log(event.key);
+    keyEvents.forEach(({ key, keyEvent }) => {
+      if (event.key === keyMapper[key]) keyEvent(event);
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', executeEvents);
+    return () => window.removeEventListener('keydown', executeEvents);
+  }, [keyEvents]);
+};
+
+export default useKeyboard;
