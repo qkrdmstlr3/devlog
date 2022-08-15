92f3ec06576132d178539c579e2231ff2dcaa75c
shellboy <qkrdmstlr3@naver.com>
2022-08-15 15:55:04 +0900

    feat: SelectBox에 키보드 조작 구현

--- src/components/ui/SelectBox/index.tsx
+++ src/components/ui/SelectBox/index.tsx
@@ -1,29 +1,60 @@
 // Dependencies
-import React from 'react';
-import { Link } from 'gatsby';
+import React, { useState } from 'react';
+import { navigate } from 'gatsby';
 import * as Style from './styled';
 
 // Components
 import BorderBox from '../BorderBox';
 import { SELECT_OPTION_TYPE } from '../../../hooks/useGame';
+import useKeyboard from '../../../utils/useKeyboard';
 
 interface SelectBoxProps {
   onClickOption: (type: SELECT_OPTION_TYPE) => void;
 }
 
 function SelectBox({ onClickOption }: SelectBoxProps) {
+  const selector = [
+    [
+      { text: '싸우기', onClick: () => onClickOption('fight') },
+      { text: '글목록', onClick: () => navigate('/') },
+    ],
+    [
+      { text: '포켓몬', onClick: () => onClickOption('pokemon') },
+      { text: '가방', onClick: () => {} },
+    ],
+  ];
+
+  const [coor, setCoor] = useState([0, 0]);
+  useKeyboard({
+    keyEvents: [
+      { key: 'ArrowUp', keyEvent: () => setCoor([Math.max(0, coor[0] - 1), coor[1]]) },
+      { key: 'ArrowDown', keyEvent: () => setCoor([Math.min(1, coor[0] + 1), coor[1]]) },
+      { key: 'ArrowRight', keyEvent: () => setCoor([coor[0], Math.min(1, coor[1] + 1)]) },
+      { key: 'ArrowLeft', keyEvent: () => setCoor([coor[0], Math.max(0, coor[1] - 1)]) },
+      { key: 'Space', keyEvent: () => selector[coor[0]][coor[1]].onClick() },
+    ],
+  });
+
+  const mouseEnterHandler = (row: number, col: number) => setCoor([row, col]);
+
   return (
     <Style.Wrapper>
       <BorderBox height="100%" />
       <Style.NavContainer>
         <BorderBox width="100%" height="100%">
           <Style.Nav>
-            <Style.NavItem onClick={() => onClickOption('fight')}>싸우기</Style.NavItem>
-            <Style.NavItem>
-              <Link to="/">글목록</Link>
-            </Style.NavItem>
-            <Style.NavItem onClick={() => onClickOption('pokemon')}>포켓몬</Style.NavItem>
-            <Style.NavItem>가방</Style.NavItem>
+            {selector.map((row, rowIndex) =>
+              row.map((col, colIndex) => (
+                <Style.NavItem
+                  key={col.text}
+                  onMouseEnter={() => mouseEnterHandler(rowIndex, colIndex)}
+                  onClick={col.onClick}
+                >
+                  {coor[0] === rowIndex && coor[1] === colIndex && <Style.Select>▶</Style.Select>}
+                  {col.text}
+                </Style.NavItem>
+              ))
+            )}
           </Style.Nav>
         </BorderBox>
       </Style.NavContainer>
--- src/components/ui/SelectBox/styled.ts
+++ src/components/ui/SelectBox/styled.ts
@@ -8,17 +8,6 @@ export const Wrapper = styled.div`
   height: 35%;
 `;
 
-export const Nav = styled.nav`
-  display: flex;
-  align-content: space-between;
-  flex-wrap: wrap;
-  height: 100%;
-  padding: 20px;
-  ${mq('sm')} {
-    padding: 3px;
-  }
-`;
-
 export const NavContainer = styled.div`
   position: absolute;
   width: 52%;
@@ -29,13 +18,31 @@ export const NavContainer = styled.div`
   padding-left: 2%;
 `;
 
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  height: 100%;
+  padding: 25px;
+  ${mq('sm')} {
+    padding: 3px;
+  }
+`;
+
 export const NavItem = styled.h2`
+  position: relative;
   display: inline-block;
-  width: 50%;
+  width: 40%;
   font-family: 'NeoDunggeunmo', sans-serif;
-  font-size: 40px;
+  font-size: 38px;
   cursor: pointer;
   ${mq('sm')} {
     font-size: 1rem;
   }
+`;
+
+export const Select = styled.span`
+  position: absolute;
+  margin-right: 15px;
+  left: -38px;
 `;
